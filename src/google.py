ad115a2115bbb2ca7b7493966d18e3b22a1efed8
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 18:37:02 +0900

    feat(google.py): Screenshot multiple pages

--- src/google.py
+++ src/google.py
@@ -15,18 +15,29 @@ class GoogleKeywordScreenShooter:
         search_bar.send_keys(Keys.ENTER)
 
         try:
-            results = self.driver.find_element_by_id("rso").find_elements_by_class_name(
-                "g"
-            )
-            for index, result in enumerate(results):
-                class_name = result.get_attribute("class")
-                if "ymu2Hb" not in result.find_element_by_xpath(
-                    "../../.."
-                ).get_attribute("class"):
-                    replaced_search_term = self.search_term.replace(" ", "_")
-                    result.screenshot(
-                        f"../screenshots/{replaced_search_term}x{index}.png"
-                    )
+            next_page_button = self.driver.find_element_by_id("pnnext")
+
+            count = 0
+            while next_page_button:
+                if count > 9:
+                    break
+
+                results = self.driver.find_element_by_id("rso").find_elements_by_class_name(
+                    "g"
+                )
+                for index, result in enumerate(results):
+                    class_name = result.get_attribute("class")
+                    if "ymu2Hb" not in result.find_element_by_xpath(
+                        "../../.."
+                    ).get_attribute("class"):
+                        replaced_search_term = self.search_term.replace(" ", "_")
+                        result.screenshot(
+                            f"../screenshots/{replaced_search_term}x{count}x{index}.png"
+                        )
+
+                next_page_button.click()
+                next_page_button = self.driver.find_element_by_id("pnnext")
+                count += 1
         except AttributeError:
             print("No results")
 
