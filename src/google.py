3e6c7c8ae4ab189fc2026db07a4ef00350c430af
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 13:26:35 +0900

    feat(google.py): Locate search bar

--- src/google.py
+++ src/google.py
@@ -3,3 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://google.com")
+
+search_bar = driver.find_element_by_class_name("gLFyf")
+search_bar.send_keys("hello!")
+
+driver.quit()
