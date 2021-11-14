bef8253fcfdac50ccbd6e4943f935a5bc468370d
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 13:32:52 +0900

    feat(google.py): Enter key and get elements of class g

--- src/google.py
+++ src/google.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -6,5 +7,11 @@ driver.get("https://google.com")
 
 search_bar = driver.find_element_by_class_name("gLFyf")
 search_bar.send_keys("hello!")
+search_bar.send_keys(Keys.ENTER)
 
-driver.quit()
+results = driver.find_elements_by_class_name("g")
+print(results)
+
+# driver.quit()
+while True:
+    pass
