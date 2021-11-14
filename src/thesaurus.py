236ab1b5cbccedeb9583f348618a77da30c9be04
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 13:01:41 +0900

    feat(thesaurus.py): Handle AttributeError if synonyms not found

--- src/thesaurus.py
+++ src/thesaurus.py
@@ -5,23 +5,34 @@ from bs4 import BeautifulSoup
 BASE_URL = "https://www.thesaurus.com/"
 
 
-def _get_synonyms(url: str) -> list:
+def _get_synonyms_from(url: str) -> list:
+    """Get synonyms from the given url.
+
+    :param url: url with search term
+    :return: empty list if synonyms not found
+    """
     req = requests.get(url)
     soup = BeautifulSoup(req.text, "html.parser")
-    synonym_container = soup.find("div", {"id": "meanings"})
-    lis = synonym_container.find_all("li")
+    try:
+        synonym_container = soup.find("div", {"id": "meanings"})
+        lis = synonym_container.find_all("li")
 
-    synonyms = []
-    for li in lis:
-        synonyms.append(li.text)
-
-    return synonyms
+        synonyms = []
+        for li in lis:
+            synonyms.append(li.text)
+        return synonyms
+    except AttributeError:
+        return []
 
 
 def get_synonyms() -> list:
+    """Get synonyms of a user input
+
+    :return: list of synonyms
+    """
     search_term = input("Search: ").lower()
     search_url = f"{BASE_URL}browse/{search_term}"
-    synonyms = _get_synonyms(search_url)
+    synonyms = _get_synonyms_from(search_url)
     return synonyms
 
 
