530adcdcd229ca5bfc4d1af199bb3179b39b76c6
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-14 12:33:07 +0900

    feat(thesaurus.py): Complete scraping thesaurus

---
+++ src/thesaurus.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+BASE_URL = "https://www.thesaurus.com/"
+
+
+def _get_synonyms(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    synonym_container = soup.find("div", {"id": "meanings"})
+    lis = synonym_container.find_all("li")
+
+    synonyms = []
+    for li in lis:
+        synonyms.append(li.text)
+
+    return synonyms
+
+
+def get_synonyms():
+    search_term = input("Search: ").lower()
+    search_url = f"{BASE_URL}browse/{search_term}"
+    synonyms = _get_synonyms(search_url)
+    print(synonyms)
+    return synonyms
+
+
+if __name__ == "__main__":
+    get_synonyms()
