7f61f39a1149b4c0684832e7efdc8e4b3c15df6a
Nayeon <dev.nayeon.shin@gmail.com>
2021-11-21 21:51:08 +0900

    refactor(word_cloud.py): Rename hashtag_cloud to cloud

--- src/word_cloud.py
+++ src/word_cloud.py
@@ -5,10 +5,10 @@ from wordcloud import WordCloud as wc
 
 def create_cloud(results):
     # create word cloud object
-    hashtag_cloud = wc(background_color="black", height=1000, width=1000, max_words=10)
-    hashtag_cloud.generate_from_frequencies(results)
+    cloud = wc(background_color="black", height=1000, width=1000, max_words=10)
+    cloud.generate_from_frequencies(results)
 
     # show the cloud
-    plt.imshow(hashtag_cloud)
+    plt.imshow(cloud)
     plt.axis("off")
     plt.show()
